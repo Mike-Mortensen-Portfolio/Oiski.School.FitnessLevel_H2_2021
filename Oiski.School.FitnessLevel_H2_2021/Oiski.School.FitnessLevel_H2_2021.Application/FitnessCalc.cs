3c9f32365046221c1ee6d575870f9f8e6b838c5b
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-01-21 12:12:09 +0100

    VO2 Max Score - Implemented the ability to calculate a persons VO2 Max score in the FitnessCalc class

--- Oiski.School.FitnessLevel_H2_2021/Oiski.School.FitnessLevel_H2_2021.Application/FitnessCalc.cs
+++ Oiski.School.FitnessLevel_H2_2021/Oiski.School.FitnessLevel_H2_2021.Application/FitnessCalc.cs
@@ -23,12 +23,21 @@ namespace Oiski.School.FitnessLevel_H2_2021.Application
         public int MaxHeartRate { get; set; }
 
         /// <summary>
-        /// Calculates the Fitness Level of a person
+        /// Calculates the <strong>Fitness Level</strong> of a person based on his/her <see cref="MaxHeartRate"/> and <see cref="RestingHeartRate"/>
         /// </summary>
-        /// <returns>The Fitness Level for a person based on his/her <see cref="MaxHeartRate"/> and <see cref="RestingHeartRate"/></returns>
+        /// <returns>An <see langword="int"/> value rounded to the nearest integer that represents a persons <strong>Fitness Level</strong></returns>
         public int GetFitnessLevel ()
         {
             return ( int ) Math.Round(( MaxHeartRate / RestingHeartRate ) * 15.3);
+        }
+
+        /// <summary>
+        /// Calculates a person <strong>VO2 Max Score</strong> based on <see cref="GetFitnessLevel"/> and the persons <see cref="Weight"/>
+        /// </summary>
+        /// <returns>A <see langword="decimal"/> value truncated to 1 decimal point that represents a persons <strong>VO2 Max Score</strong></returns>
+        public decimal GetVO2Max ()
+        {
+            return Math.Truncate(( GetFitnessLevel() * Weight ) / 1000);
         }
     }
 }
