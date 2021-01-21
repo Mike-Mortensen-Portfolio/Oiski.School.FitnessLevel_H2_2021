e0cb7e8e77145984cafdb705b913ad713bbe0b2d
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-01-21 13:01:03 +0100

    Main Menu The Intergration of a Basic Main Menu

--- Oiski.School.FitnessLevel_H2_2021/Oiski.School.FitnessLevel_H2_2021.Application/Program.cs
+++ Oiski.School.FitnessLevel_H2_2021/Oiski.School.FitnessLevel_H2_2021.Application/Program.cs
@@ -6,7 +6,29 @@ namespace Oiski.School.FitnessLevel_H2_2021.Application
     {
         static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int menuIndex = 0;
+            do
+            {
+                switch ( menuIndex )
+                {
+                    #region Main Menu
+                    case 0:
+                        do
+                        {
+                            Console.Clear();
+                            Console.WriteLine("1) Data Collection");
+                            Console.WriteLine("2) Results");
+                        } while ( !int.TryParse(Console.ReadLine(), out menuIndex) && menuIndex > 0 && menuIndex < 3 );
+                        break;
+                        #endregion
+
+                        #region Data Menu
+                        #endregion
+
+                        #region Result menu
+                        #endregion
+                }
+            } while ( true );
         }
     }
 }
