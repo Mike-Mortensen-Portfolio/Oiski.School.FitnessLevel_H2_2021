0cb2b5a159c52781a24d95118100ec4b9c3e4128
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-01-21 20:12:46 +0100

    Imported Oiski Engine + Setup of Engine specifications

--- Oiski.School.FitnessLevel_H2_2021/Oiski.School.FitnessLevel_H2_2021.Application/Program.cs
+++ Oiski.School.FitnessLevel_H2_2021/Oiski.School.FitnessLevel_H2_2021.Application/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Oiski.ConsoleTech.Engine;
+using Oiski.ConsoleTech.Engine.Color.Rendering;
 
 namespace Oiski.School.FitnessLevel_H2_2021.Application
 {
@@ -6,95 +8,28 @@ namespace Oiski.School.FitnessLevel_H2_2021.Application
     {
         static void Main (string[] args)
         {
+            #region Engine Setup
+            Console.SetWindowSize(60, 30);
+            ColorRenderer renderer = new ColorRenderer();
+            renderer.DefaultColor = new RenderColor(ConsoleColor.Red, ConsoleColor.Black);
+
+            OiskiEngine.ChangeRenderer(renderer);
+            OiskiEngine.Run();
+            #endregion
+
+            RenderColor textColor = new RenderColor(ConsoleColor.Cyan, ConsoleColor.Black);
+            RenderColor borderColor = new RenderColor(ConsoleColor.DarkBlue, ConsoleColor.Black);
+
             FitnessCalc fitness = new FitnessCalc();
-            int menuIndex = 0;
-            do
-            {
-                Console.Clear();
-                switch ( menuIndex )
-                {
-                    #region Main Menu
-                    case 0:
-                        do
-                        {
-                            Console.Clear();
-                            Console.WriteLine("1) Data Collection");
-                            Console.WriteLine("2) Results");
-                        } while ( !int.TryParse(Console.ReadLine(), out menuIndex) && menuIndex > 0 && menuIndex < 3 );
-                        break;
-                    #endregion
 
-                    #region Data Menu
-                    case 1:
+            #region Main Menu
+            #endregion
 
-                        decimal weight;
-                        int restingHeartRate;
-                        int maxHeartRate;
+            #region Data Menu
+            #endregion
 
-                        do
-                        {
-                            Console.Write("Enter Weight (kg): ");
-                        } while ( !decimal.TryParse(Console.ReadLine(), out weight) );
-
-                        do
-                        {
-                            Console.Write("Enter Resting Heart Rate (BPM): ");
-                        } while ( !int.TryParse(Console.ReadLine(), out restingHeartRate) );
-
-                        do
-                        {
-                            Console.Write("Enter Max Heart Rate (BPM): ");
-                        } while ( !int.TryParse(Console.ReadLine(), out maxHeartRate) );
-
-                        fitness.Weight = weight;
-                        fitness.RestingHeartRate = restingHeartRate;
-                        fitness.MaxHeartRate = maxHeartRate;
-
-                        Console.WriteLine("Press Any Key...");
-                        Console.ReadKey();
-                        menuIndex = 0;
-                        break;
-                    #endregion
-
-                    #region Result Menu
-                    case 2:
-                        Console.WriteLine($"Weight: {fitness.Weight}kg");
-                        Console.WriteLine($"Resting Heart Rate: {fitness.RestingHeartRate} BPM");
-                        Console.WriteLine($"Max Heart Rate: {fitness.MaxHeartRate} BPM");
-
-                        try
-                        {
-                            Console.WriteLine($"Fitness Level: {fitness.GetFitnessLevel()} ml/kg/min");
-                        }
-                        catch ( DivideByZeroException )
-                        {
-                            Console.Write($"Fitness Level: ");
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write($"Error\n");
-                            Console.ResetColor();
-                        }
-
-                        try
-                        {
-                            Console.WriteLine($"VO2 Max Score: {fitness.GetVO2Max()} l/ml");
-                        }
-                        catch ( DivideByZeroException )
-                        {
-                            Console.Write($"VO2 Max Score: ");
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write($"Error");
-                            Console.ResetColor();
-                        }
-
-                        Console.WriteLine();
-
-                        Console.WriteLine("Press Any Key...");
-                        Console.ReadKey();
-                        menuIndex = 0;
-                        break;
-                        #endregion 
-                }
-            } while ( true );
+            #region Result Menu
+            #endregion
         }
     }
 }
